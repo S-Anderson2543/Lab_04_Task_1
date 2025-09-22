62a160d9308577635f6e4e50677c5674bfeff06f
S-Anderson2543 <ander4s7@mail.uc.edu>
2025-09-22 13:23:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    double purchasePrice = 25.35;
+    final double SALES_TAX = 0.05;
+    double finalPrice = purchasePrice * SALES_TAX;
+
+    System.out.println("Your final price is $" + finalPrice);
+    }
+
